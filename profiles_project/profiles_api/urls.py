b28781b0632d60b3fa3977781bb902c934bc66eb
ArtusU <artusrock@hotmail.com>
2020-08-12 18:02:50 +0100

    CRUD added to ViewSet

--- profiles_project/profiles_api/urls.py
+++ profiles_project/profiles_api/urls.py
@@ -1,6 +1,14 @@
 from django.conf.urls import url
+from django.urls import include
+
+from rest_framework.routers import DefaultRouter
+
 from . import views
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
 
 urlpatterns = [
     url('hello-view/', views.HelloApiView.as_view()),
+    url('', include(router.urls))
 ]
